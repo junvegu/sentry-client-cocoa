d78035c7e86ae2933ad9f5c77ea2cb6ff56b4ec1
Junior <juniorquegu@gmail.com>
2024-11-21 22:25:31 -0500

    spm

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "sentry_client_cocoa",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "sentry_client_cocoa",
+            targets: ["sentry_client_cocoa"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.36.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "sentry_client_cocoa",
+            path: "./sentry_client_cocoa.xcframework"
+        )
+    ]
+)
